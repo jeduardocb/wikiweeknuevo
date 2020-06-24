f07602bbf21e3505d0618047f4f80ff96e8cc2e0
eduardo <A01704641@itesm.mx>
2020-06-24 00:04:45 -0500

    agregar conexion base de datos

---
+++ util.php
@@ -0,0 +1,22 @@
+<?php  
+	function conectar_bd(){
+    $servername = "mysql1008.mochahost.com";
+		$username = "dawbdorg_1704641";
+		$password = "1704641";
+		$dbname = "dawbdorg_A01704641";
+		$con = mysqli_connect($servername,$username,$password,$dbname);
+      
+		if(!$con){
+			die("conexion fallida" .mysqli_connect_error());
+		}
+		return $con;
+	}
+	function desconectar_bd($mysql){
+		mysqli_close($mysql);
+	}
+
+
+    
+   
+
+?> 
