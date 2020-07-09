72ed2618233ac6f9cd785db167f079a75755f487
eduardo <A01704641@itesm.mx>
2020-07-09 01:27:36 -0500

    agregar foto en caterogira no terminado todavia

--- addCategoria.php
+++ addCategoria.php
@@ -10,6 +10,10 @@ include_once("util.php");
         <label for="exampleInputEmail1">Nombre de la Nueva Categoria:</label>
         <input type="text" class="form-control" id="nuevaCategoria" aria-describedby="emailHelp" placeholder="Hibrido">
       </div>
+        <div class="form-group">
+        <label class="col-sm-2 control-label">Imagen:</label>
+        <input type="file" class="form-control" id="upload" name="upload"  required>
+    </div>
       <button type="button" id="add-Categoria"class="btn btn-primary">Agregar Categoria</button>
     </form>
 
