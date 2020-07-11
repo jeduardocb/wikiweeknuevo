c831a148f7ce7d259cf5af4d2ed906eae334b05d
eduardo-gomez-i <gomez.i.eduardomanuel@gmail.com>
2020-07-11 01:38:46 -0500

    cambios en estilos

--- index.php
+++ index.php
@@ -151,7 +151,7 @@ include_once("util.php");
 
 
 
-<div id="fh5co-counter" class="fh5co-bg fh5co-counter" style="background-image:url(images/img_bg_5.jpg);">
+<div id="fh5co-counter" class="fh5co-bg fh5co-counter" style="background-image:url(images/fondo3.jpg);">
 	<div class="container">
 		<div class="row">
 			<div class="display-t">
@@ -203,31 +203,6 @@ include_once("util.php");
 	</div>
 </div>
 
-<div id="fh5co-started">
-	<div class="container">
-		<div class="row animate-box">
-			<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-				<h2>Newsletter</h2>
-				<p>Just stay tune for our latest Product. Now you can subscribe</p>
-			</div>
-		</div>
-		<div class="row animate-box">
-			<div class="col-md-8 col-md-offset-2">
-				<form class="form-inline">
-					<div class="col-md-6 col-sm-6">
-						<div class="form-group">
-							<label for="email" class="sr-only">Email</label>
-							<input type="email" class="form-control" id="email" placeholder="Email">
-						</div>
-					</div>
-					<div class="col-md-6 col-sm-6">
-						<button type="submit" class="btn btn-default btn-block">Subscribe</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-</div>
 <?php
 include("_footer.html");
 ?>
