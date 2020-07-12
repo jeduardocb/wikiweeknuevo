60a0b589b4be4928db35d466149285dc5c196a6c
eduardo <A01704641@itesm.mx>
2020-07-12 16:36:54 -0500

    agregar cambios

--- addCepa.php
+++ addCepa.php
@@ -21,7 +21,7 @@ if(isset($_SESSION["mensaje"])){
 <div class="container">
   <form id="addCepa" action="controlador_addCepa.php" method="POST" enctype="multipart/form-data">
     <div class="form-group ">
-      <label for="usr">Nombre:</label>
+      <label for="usr" >Nombre:</label>
       <input type="text" class="form-control " id="name" name="name" required>
     </div>
     <div class="form-group ">
@@ -30,30 +30,35 @@ if(isset($_SESSION["mensaje"])){
     </div>
 
     <div class="form-group">
-      <h5>Categoria</h5>
+      <label>Categoria</label>
       <?= crear_select("id", "nombre", "categoria") ?>
     </div>
     <div class="form-group">
-      <h5>Terpenos</h5>
+      <label>Terpenos</label>
       <?= getTerpenos() ?>
     </div>
 
-    <div class="input-group">
-      <h5>CBD</h5>
+    <div class="form-group">
+      <label>CBD</label>
+        <p >Mínimo:</p>
       <input type="number" id="cbdmin" name="cbdmin" class="form-control" placeholder="0" min="0" max="100" step="0.01" required>
+        
+        <p >Máximo:</p>
       <input type="number" id="cbdmax" name="cbdmax" class="form-control" placeholder="10" min="0" max="100" step="0.01">
+        
     </div>
 
 
     <div class="input-group">
 
-      <h5>THC</h5>
-
+      <label>THC</label>
+        <p >Mínimo:</p>
       <input type="number" id="thcmin" name="thcmin" class="form-control" placeholder="0" min="0" max="100" step="0.01" required>
+        <p >Máximo:</p>
       <input type="number" id="thcmax" name="thcmax" class="form-control" placeholder="10" min="0" max="100" step="0.01">
     </div>
     <div class="form-group">
-      <h5>Crecimiento</h5>
+      <label>Crecimiento</label>
       <label for="sel1">Difficulty:</label>
       <select class="form-control" id="dificultad" name="dificultad" required>
         <option value="facil">Facil</option>
@@ -63,9 +68,8 @@ if(isset($_SESSION["mensaje"])){
 
       <label for="sel1">Altura (pulgadas):</label>
       <select class="form-control" id="altura" name="altura" required>
-        <option value="< 30">
-          < 30</option> <option value="30 - 78">30 - 78
-        </option>
+        <option value="< 30"> < 30   </option> 
+          <option value="30 - 78">30 - 78</option>
         <option value="> 78">> 78</option>
       </select>
       <label for="sel1">Rendimiento (Oz/Ft)^2:</label>
