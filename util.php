3500ed6ecf48c1cea2108023ff3ca9b01bdda0c7
eduardo-gomez-i <gomez.i.eduardomanuel@gmail.com>
2020-07-16 01:21:21 -0500

    agregar edit cepa, falta terminar

--- util.php
+++ util.php
@@ -695,6 +695,7 @@ function getListadoCepas(){
   if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
+      $idweed=  $row["weedId"];
       echo '<tr>
                 <td data-th="Product">
                     <div class="row">
@@ -702,18 +703,131 @@ function getListadoCepas(){
                             <h4 class="nomargin">'.$row['weedNombre'].'</h4>
                         </div>
                         <div class="col-sm-6 hidden-xs">
-                          <img src="images/cepas/'.$row["nombre"].'" style="with: 20px;" class="img-responsive" />
+                          <img src="images/cepas/'.$row["nombre"]. '" style="with: 20px;" class="img-responsive" />
                         </div>
                     </div>
                 </td>
                 <td class="actions" data-th="">
-                    <button class="btn btn-info btn-sm"><i class="fas fa-wrench"></i></button>
+                <form id="' . $row["weedId"] . '" action="editCepa.php" method="get">
+						    <input type="hidden" name="idweed" value="' . $row["weedId"] . '">
+                      <a href="javascript:{}" onclick="document.getElementById(' . "'$idweed'" . ').submit();" class="icon">
+                        <button class="btn btn-info btn-sm"><i class="fas fa-wrench"></i></button>
+                      </a>
                     <button class=" btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                </form>
                 </td>
             </tr>';
+						
     }
   }
   desconectar_bd($con);
+}
+
+function crear_selectEdit($id, $columna_descripcion, $tabla, $categoria)
+{
+  $conion_bd = conectar_bd();
+
+
+  $resultado = '<select class="form-control" name="' . $tabla . '" id="' . $tabla . '" value=""><option disabled selected>'.$categoria.'</option>';
+
+
+  $consulta = "SELECT $id  , $columna_descripcion  FROM $tabla";
+  $resultados = $conion_bd->query($consulta);
+  while ($row = mysqli_fetch_array($resultados, MYSQLI_BOTH)) {
+
+    if ($row["$columna_descripcion"] != $categoria) {
+      $resultado .= '<option value="' . $row["$id"] . '">' . $row["$columna_descripcion"] . '</option>';
+    }
+  }
+
+  desconectar_bd($conion_bd);
+  $resultado .=  '</select>';
+  return $resultado;
+}
+
+function formEditCepa($idweed, $nombre, $descripcion, $categoria){
+  $con = conectar_bd();
+  $sql = "SELECT weed.nombre AS weedNombre, weed.id AS weedId
+          FROM weed WHERE weed.id = $idweed";
+          $result = $con->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+    }
+  }
+  echo '<form id="addCepa" action="controlador_editCepa.php" method="GET" enctype="multipart/form-data">
+        <div class="form-group ">
+            <label for="usr">Nombre:</label>
+            <input type="text" class="form-control " id="name" name="name" value="'. $nombre . '" required>
+        </div>
+        <div class="form-group ">
+            <label for="usr">Descripcion:</label>
+            <textarea type="textarea" class="form-control " id="descripcion" name="descripcion" required>' . $descripcion . '</textarea>
+        </div>
+
+        <div class="form-group">
+            <label>Categoria</label>
+            '. crear_selectEdit("id", "nombre", "categoria", $categoria).'
+        </div>
+        <div class="form-group">
+            <label>Terpenos</label>
+            <?= getTerpenos() ?>
+        </div>
+
+        <div class="input-group">
+            <label>CBD</label>
+            <p>Mínimo:</p>
+            <input type="number" id="cbdmin" name="cbdmin" class="form-control" placeholder="0" min="0" max="100" step="0.01" required>
+            <p>Máximo:</p>
+            <input type="number" id="cbdmax" name="cbdmax" class="form-control" placeholder="10" min="0" max="100" step="0.01">
+        </div>
+
+
+        <div class="input-group">
+
+            <label>THC</label>
+            <p>Mínimo:</p>
+            <input type="number" id="thcmin" name="thcmin" class="form-control" placeholder="0" min="0" max="100" step="0.01" required>
+            <p>Máximo:</p>
+            <input type="number" id="thcmax" name="thcmax" class="form-control" placeholder="10" min="0" max="100" step="0.01">
+        </div>
+        <div class="form-group">
+            <label>Crecimiento</label>
+            <label for="sel1">Difficulty:</label>
+            <select class="form-control" id="dificultad" name="dificultad" required>
+                <option value="facil">Facil</option>
+                <option value="moderado">Moderado</option>
+                <option value="dificil">Dificil</option>
+            </select>
+
+            <label for="sel1">Altura (pulgadas):</label>
+            <select class="form-control" id="altura" name="altura" required>
+                <option value="< 30">
+                    < 30</option> <option value="30 - 78">30 - 78
+                </option>
+                <option value="> 78">> 78</option>
+            </select>
+            <label for="sel1">Rendimiento (Oz/Ft)^2:</label>
+            <select class="form-control" id="rendimiento" name="rendimiento" required>
+                <option value="0.5 - 1">0.5 - 1</option>
+                <option value="1 - 3">1 - 3</option>
+                <option value="3 - 6">3 - 6</option>
+            </select>
+            <label for="sel1">Florecimiento (En Semanas):</label>
+            <select class="form-control" id="florecimiento" name="florecimiento" required>
+                <option value="7 - 9">7 - 9</option>
+                <option value="10 - 12">10 - 12</option>
+                <option value="> 12">> 12</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-2 control-label">Archivos</label>
+            <input type="file" class="form-control" id="upload" name="upload[]" multiple required>
+        </div>
+        <input type="submit" value="Upload Image" name="submit">
+
+    </form>';
 }
 
 ?> 
