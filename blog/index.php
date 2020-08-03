541a2ea047522510f6fb27548dd4a00ef148a142
eduardo-gomez-i <gomez.i.eduardomanuel@gmail.com>
2020-08-03 01:06:59 -0500

    agregar blog

---
+++ blog/index.php
@@ -0,0 +1,162 @@
+<?php
+include('header.html');
+include('nav.html');
+?>
+
+<!-- Hero Section-->
+<section style="background: url(img/banner.jpg); background-size: cover; background-position: center center" class="hero">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-7">
+        <h1>wiki Blog</h1><a href="#" class="hero-link">Descubrir más</a>
+      </div>
+    </div><a href=".intro" class="continue link-scroll"><i class="fa fa-long-arrow-down"></i> Scroll Down</a>
+  </div>
+</section>
+<!-- Intro Section-->
+<section class="intro">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8">
+        <h2 class="h3">Descripción</h2>
+        <p class="text-big">Aqui va una <strong>introdicción</strong>. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderi.</p>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="featured-posts no-padding-top">
+  <div class="container">
+    <!-- Post-->
+    <div class="row d-flex align-items-stretch">
+      <div class="text col-lg-7">
+        <div class="text-inner d-flex align-items-center">
+          <div class="content">
+            <header class="post-header">
+              <div class="category"><a href="#">Cultura</a><a href="#">CBD</a></div><a href="post.html">
+                <h2 class="h4">CBD puede ayudar a tus mascotas con los fuegos artifiales</h2>
+              </a>
+            </header>
+            <p>Con julio de 4 º sobre nosotros y los fuegos artificiales organizados pantallas de haber sido cancelada en muchas ciudades y pueblos debido a las preocupaciones de coronavirus, parece que este año las personas están soplando fuera de sus propias M-80, bombas de cereza y velas romanas.
+
+              Los caninos encuentran ruidos fuertes y luces intermitentes intolerables en sus sistemas nerviosos. Los fuegos artificiales pueden ser desencadenantes y estresantes para los perros ansiosos, ya que los estallidos repentinos de sonido pueden liberar adrenalina y hormonas del estrés que les dicen a sus instintos que corran.</p>
+            <footer class="post-footer d-flex align-items-center"><a href="#" class="author d-flex align-items-center flex-wrap">
+                <div class="avatar"><img src="img/avatar-1.jpg" alt="..." class="img-fluid"></div>
+                <div class="title"><span>Dante</span></div>
+              </a>
+              <div class="date"><i class="icon-clock"></i> 2 months ago</div>
+              <div class="comments"><i class="icon-comment"></i>12</div>
+            </footer>
+          </div>
+        </div>
+      </div>
+      <div class="image col-lg-5"><img src="img/fuegos.jpg" alt="..."></div>
+    </div>
+  </div>
+</section>
+<!-- Divider Section-->
+<section style="background: url(img/divider-bg.jpg); background-size: cover; background-position: center bottom" class="divider">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7">
+        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</h2><a href="#" class="hero-link">View More</a>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Latest Posts -->
+<section class="latest-posts">
+  <div class="container">
+    <header>
+      <h2>Ultimos blogs</h2>
+      <p class="text-big">Ve nuestros ultimos blogs.</p>
+    </header>
+    <div class="row">
+      <div class="post col-md-4">
+        <div class="post-thumbnail"><a href="post.html"><img src="img/blog-1.jpg" alt="..." class="img-fluid"></a></div>
+        <div class="post-details">
+          <div class="post-meta d-flex justify-content-between">
+            <div class="date">20 May | 2020</div>
+            <div class="category"><a href="#">Legal</a></div>
+          </div><a href="post.html">
+            <h3 class="h4">¿Cuántos Gramos Por Onza De Hierba?</h3>
+          </a>
+          <p class="text-muted">No se deje confundir por la utilización cruzada de gramos y onzas para medir la hierba. Como cualquier entusiasta del cannabis sabe, una onza es mucho más hierba que un gramo . Si quieres ser técnico al respecto, hay 28.34952 gramos por onza.</p>
+        </div>
+      </div>
+      <div class="post col-md-4">
+        <div class="post-thumbnail"><a href="post.html"><img src="img/blog-2.jpg" alt="..." class="img-fluid"></a></div>
+        <div class="post-details">
+          <div class="post-meta d-flex justify-content-between">
+            <div class="date">20 May | 2020</div>
+            <div class="category"><a href="#">Salud</a></div>
+          </div><a href="post.html">
+            <h3 class="h4">Todo Lo Que Hay Que Saber Sobre El Cuidado De La Piel Con CBD</h3>
+          </a>
+          <p class="text-muted">El CBD tiene efectos antioxidantes y antiinflamatorios que pueden ayudar a abordar problemas de la piel como sequedad, daño por radicales libres e inflamación. El CBD también puede beneficiar a las personas que padecen acné porque puede reducir la cantidad de sebo producido. Además, algunos estudios indican que es un tratamiento efectivo para problemas como el eccema y la psoriasis.</p>
+        </div>
+      </div>
+      <div class="post col-md-4">
+        <div class="post-thumbnail"><a href="post.html"><img src="img/blog-3.jpg" alt="..." class="img-fluid"></a></div>
+        <div class="post-details">
+          <div class="post-meta d-flex justify-content-between">
+            <div class="date">20 May | 2020</div>
+            <div class="category"><a href="#">Salud</a></div>
+          </div><a href="post.html">
+            <h3 class="h4">Aventuras En El Cannaturismo</h3>
+          </a>
+          <p class="text-muted">Los entusiastas del cannabis tienen mucho que agradecer y esperar el próximo año. Canadá celebró recientemente un año de cannabis totalmente legalizado, se abrieron dispensarios de uso para adultos en Massachusetts, y el uso de cannabis para adultos se convirtió en legal en Illinois , con Michigan no muy lejos. A medida que los cambios en las políticas en los Estados Unidos continúan expandiendo el acceso al cannabis, la industria en su conjunto continúa aplastando los estigmas y los tabúes andrajosos.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Newsletter Section-->
+<section class="newsletter no-padding-top">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h2>Suscribete para mas contenido</h2>
+        <p class="text-big">Texto de prueba</p>
+      </div>
+      <div class="col-md-8">
+        <div class="form-holder">
+          <form action="#">
+            <div class="form-group">
+              <input type="email" name="email" id="email" placeholder="correo@correo.com">
+              <button type="submit" class="submit">Suscribirse</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Gallery Section-->
+<section class="gallery no-padding">
+  <div class="row">
+    <div class="mix col-lg-3 col-md-3 col-sm-6">
+      <div class="item"><a href="img/gallery-1.jpg" data-fancybox="gallery" class="image"><img src="img/gallery-1.jpg" alt="..." class="img-fluid">
+          <div class="overlay d-flex align-items-center justify-content-center"><i class="icon-search"></i></div>
+        </a></div>
+    </div>
+    <div class="mix col-lg-3 col-md-3 col-sm-6">
+      <div class="item"><a href="img/gallery-2.jpg" data-fancybox="gallery" class="image"><img src="img/gallery-2.jpg" alt="..." class="img-fluid">
+          <div class="overlay d-flex align-items-center justify-content-center"><i class="icon-search"></i></div>
+        </a></div>
+    </div>
+    <div class="mix col-lg-3 col-md-3 col-sm-6">
+      <div class="item"><a href="img/gallery-3.jpg" data-fancybox="gallery" class="image"><img src="img/gallery-3.jpg" alt="..." class="img-fluid">
+          <div class="overlay d-flex align-items-center justify-content-center"><i class="icon-search"></i></div>
+        </a></div>
+    </div>
+    <div class="mix col-lg-3 col-md-3 col-sm-6">
+      <div class="item"><a href="img/gallery-4.jpg" data-fancybox="gallery" class="image"><img src="img/gallery-4.jpg" alt="..." class="img-fluid">
+          <div class="overlay d-flex align-items-center justify-content-center"><i class="icon-search"></i></div>
+        </a></div>
+    </div>
+  </div>
+</section>
+
+<?php
+include('footer.html');
+?>
